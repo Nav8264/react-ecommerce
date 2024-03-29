1ed9e724d8cdb80c67823a5268b75390a27faac8
Navneet <navneet.singh0863@gmail.com>
2024-03-29 16:23:41 +0530

    done till login temp

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import Navbar from "../features/Navbar/navbar";
+import ProductList from "../features/product-list/ProductList";
+
+function Home() {
+  return (
+    <div>
+      <Navbar>
+        <ProductList></ProductList>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Home;
